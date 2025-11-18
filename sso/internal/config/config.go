3521097c6bb95de9c19203eaaf5aeb37463aaaa8
Aleks12387 <aleksejcuk344@gmail.com>
2025-11-18 22:08:38 +0300

    start tests

--- sso/internal/config/config.go
+++ sso/internal/config/config.go
@@ -9,40 +9,49 @@ import (
 )
 
 type Config struct {
-	Env         string        `yaml:"env" env-default:"local"` //strike tag
-	StoragePath string        `yaml:"storage_path" env-required:"./data"`
-	TokenTTL    time.Duration `yaml:"token_ttl" env-default:"1h"`
-	GRPC        GRPCconfig    `yaml:"grpc"`
+	Env            string     `yaml:"env" env-default:"local"`
+	StoragePath    string     `yaml:"storage_path" env-required:"true"`
+	GRPC           GRPCConfig `yaml:"grpc"`
+	MigrationsPath string
+	TokenTTL       time.Duration `yaml:"token_ttl" env-default:"1h"`
 }
-type GRPCconfig struct {
+
+type GRPCConfig struct {
 	Port    int           `yaml:"port"`
 	Timeout time.Duration `yaml:"timeout"`
 }
 
 func MustLoad() *Config {
-	path := fetchConfigPath()
-	if path == "" {
-		panic("config file path is empty")
+	configPath := fetchConfigPath()
+	if configPath == "" {
+		panic("config path is empty")
 	}
 
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		panic("config file not found: " + path)
+	return MustLoadPath(configPath)
+}
+
+func MustLoadPath(configPath string) *Config {
+	// check if file exists
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		panic("config file does not exist: " + configPath)
 	}
 
 	var cfg Config
 
-	if err := cleanenv.ReadConfig(path, &cfg); err != nil {
-		panic("failed to read config: " + err.Error())
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		panic("cannot read config: " + err.Error())
 	}
 
 	return &cfg
 }
 
+// fetchConfigPath fetches config path from command line flag or environment variable.
+// Priority: flag > env > default.
+// Default value is empty string.
 func fetchConfigPath() string {
 	var res string
 
-	// --config="path/to/config.yaml
-	flag.StringVar(&res, "config", "", "path o config file")
+	flag.StringVar(&res, "config", "", "path to config file")
 	flag.Parse()
 
 	if res == "" {
